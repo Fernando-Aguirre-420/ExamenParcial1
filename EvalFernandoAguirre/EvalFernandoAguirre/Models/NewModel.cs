62bd69148af3ab9fe300ff4b677bdcb1c4661493
Luis Aguirre <a2019119684@estudiantes.upsa.edu.bo>
2020-09-18 21:49:09 -0400

    finalizado campos para crud

---
+++ EvalFernandoAguirre/EvalFernandoAguirre/Models/NewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvalFernandoAguirre.Models
+{
+    public class NewModel
+    {
+        public string name { get; set; }
+        public string capital { get; set; }
+        public int population { get; set; }
+        public string latlng { get; set; }
+        public string timezones { get; set; }
+        public string currencies { get; set; }
+        public int flag { get; set; }
+    }
+}
